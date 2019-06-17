7e9376feb0fefac9908b8a76a989e27b391f74e4
rakibulislam01 <mrakibul70@gmail.com>
2019-06-17 19:28:47 +0600

    add ajax create view

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.urls import path
 from django.views.generic.base import RedirectView
 
-from .views import (TweetListAPIView)
+from .views import (TweetListAPIView,
+                    TweetCreateAPIView)
 
 app_name = 'tweets-api'
 urlpatterns = [
-    url('', TweetListAPIView.as_view(), name='list')  # api/tweet/
+    url('list', TweetListAPIView.as_view(), name='list'),  # api/tweet/
+    url('create', TweetCreateAPIView.as_view(), name='create')  # api/tweet/
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
