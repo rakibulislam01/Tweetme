e537fadae9db3235699da8bb8e0b244781cade5b
rakibulislam01 <mrakibul70@gmail.com>
2019-07-20 23:51:29 +0600

    Model Manager for Following

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,28 @@ from django.conf import settings
 from django.db import models
 
 
+class UserProfileManager(models.Manager):
+    use_for_related_fields = True
+
+    def all(self):
+        qs = self.get_queryset().all()
+        try:
+            if self.instance:
+                qs = qs.exclude(user=self.instance)
+        except:
+            pass
+        return qs
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=True, related_name='profile')
     following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
 
+    object = UserProfileManager()
+
     def __str__(self):
         return str(self.following.all().count())
+
+    def get_following(self):
+        users = self.following.all()
+        return users.exclude(username=self.user.username)
